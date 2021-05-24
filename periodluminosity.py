16e1105bbc2abf16390d9026051c1089c7890124
Michael Chen <45020248+fl4mx@users.noreply.github.com>
2021-05-24 11:44:46 +1000

    Update periodluminosity.py
    update

--- periodluminosity.py
+++ periodluminosity.py
@@ -135,6 +135,35 @@ def rrlyr_PL(BLG_metadata, LMC_metadata, SMC_metadata):
         plt.show
         plt.savefig((outputdir + "/" + (plotname) + ".png"), format="png", dpi=1200)
 
+    # plotter for specific type, region
+    # def specificscatterplot(iv, type, region, xtitle, ytitle, plotname, size):
+    def specificscatterplot(iv, specificarray, region, type, xtitle, ytitle, size):
+        #arrayname = str(str(region) + "_" + str(type))
+        #specificarray = eval(arrayname)
+        #specificarray = eval(str(region) + "_" + str(type))
+        #specificarray = eval("region + \"_\" + type")
+        #print(type(specificarray))
+        #print(specificarray[0])
+        plotname = str(xtitle) + " vs " + str(ytitle) + " of " + str(type) + " in " + str(region)
+
+        fig = plt.figure(figsize=(10, 5))
+        fig.suptitle(str(plotname), fontsize=20, fontweight='bold', y=0.96)
+        ax = fig.add_subplot(111)
+        fig.subplots_adjust(left=0.1, right=0.98, top=0.87, bottom=0.1)
+        ax.set_xlabel(xtitle)
+        ax.set_ylabel(ytitle)
+        ax.invert_yaxis()
+        # plot
+        # specificarray = globals()[region + "_" + type]
+        if iv == "i":
+            #ax.scatter((globals()[region + "_" + type][0]), (globals()[region + "_" + type][1]), s = size, color = "#332288")
+            ax.scatter(specificarray[0], specificarray[1], s=size, color="#332288")
+        if iv == "v":
+            #ax.scatter((globals()[region + "_" + type][0]), (globals()[region + "_" + type][2]), s=size, color="#332288")
+            ax.scatter(specificarray[0], specificarray[2], s=size, color="#332288")
+        #plt.show
+        plt.savefig((outputdir + "/" + (plotname) + ".png"), format="png", dpi=1200)
+
 
 
     # reading columns of BLG, LMC, SMC
@@ -208,8 +237,18 @@ def rrlyr_PL(BLG_metadata, LMC_metadata, SMC_metadata):
     # allstars = np.append(allstars, RRc, axis=1)
     # allstars = np.append(allstars, RRd, axis=1)
 
-
-
+    """
+    BLG_RRab = BLG_RRab.tolist()
+    BLG_RRc = BLG_RRc.tolist()
+    BLG_RRd = BLG_RRd.tolist()
+    LMC_RRab = LMC_RRab.tolist()
+    LMC_RRc = LMC_RRc.tolist()
+    LMC_RRd = LMC_RRd.tolist()
+    SMC_RRab = SMC_RRab.tolist()
+    SMC_RRc = SMC_RRc.tolist()
+    SMC_RRd = SMC_RRd.tolist()
+    """
+    
     typescatterplot("i", "RRab", "Period", "I-Band Magnitude", "Period vs I-Band Magnitude for all RRab", 0.2)
     typescatterplot("i", "RRc", "Period", "I-Band Magnitude", "Period vs I-Band Magnitude for all RRc", 0.2)
     typescatterplot("i", "RRd", "Period", "I-Band Magnitude", "Period vs I-Band Magnitude for all RRd", 0.2)
@@ -228,4 +267,26 @@ def rrlyr_PL(BLG_metadata, LMC_metadata, SMC_metadata):
     allscatterplot("v", "Period", "V-Band Magnitude", "Period vs V-Band Magnitude for all RR Lyrae", 0.2)
 
 
+    specificscatterplot("i", BLG_RRab, "BLG", "RRab", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("i", BLG_RRc, "BLG", "RRc", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("i", BLG_RRd, "BLG", "RRd", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("i", LMC_RRab, "LMC", "RRab", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("i", LMC_RRc, "LMC", "RRc", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("i", LMC_RRd, "LMC", "RRd", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("i", SMC_RRab, "SMC", "RRab", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("i", SMC_RRc, "SMC", "RRc", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("i", SMC_RRd, "SMC", "RRd", "Period", "I-Band Magnitude", 0.1)
+    specificscatterplot("v", BLG_RRab, "BLG", "RRab", "Period", "V-Band Magnitude", 0.1)
+    specificscatterplot("v", BLG_RRc, "BLG", "RRc", "Period", "V-Band Magnitude", 0.1)
+    specificscatterplot("v", BLG_RRd, "BLG", "RRd", "Period", "V-Band Magnitude", 0.1)
+    specificscatterplot("v", LMC_RRab, "LMC", "RRab", "Period", "V-Band Magnitude", 0.1)
+    specificscatterplot("v", LMC_RRc, "LMC", "RRc", "Period", "V-Band Magnitude", 0.1)
+    specificscatterplot("v", LMC_RRd, "LMC", "RRd", "Period", "V-Band Magnitude", 0.1)
+    specificscatterplot("v", SMC_RRab, "SMC", "RRab", "Period", "V-Band Magnitude", 0.1)
+    specificscatterplot("v", SMC_RRc, "SMC", "RRc", "Period", "V-Band Magnitude", 0.1)
+    specificscatterplot("v", SMC_RRd, "SMC", "RRd", "Period", "V-Band Magnitude", 0.1)
+
+
+    
+    
 rrlyr_PL(BLG_metadata, LMC_metadata, SMC_metadata)
